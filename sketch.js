627f5d43ebe0bcf9cd7095c534683de43bfed201
LukejesusDias <89021514+LukejesusDias@users.noreply.github.com>
2021-08-17 18:45:43 +0530

    Add files via upload
    added all files

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var box=createSprite(200,200,50,30);
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+  background(30);
+if (keyDown(DOWN_ARROW)) {
+  background('red');
+}
+if (keyDown(RIGHT_ARROW)) {
+  background('blue');
+}if (keyDown(LEFT_ARROW)) {
+  background('yellow');
+}if (keyDown(UP_ARROW)) {
+  background('purple');
+}
+drawSprites()
+}
+
+
+
+
